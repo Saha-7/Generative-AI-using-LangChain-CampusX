58666fb3eca662d6c7ce376c5c2bd9956213e6b6
saha-7op <saha7pritam@gmail.com>
2025-09-05 21:03:24 +0530

    Hugging face poem

---
+++ 2. Chat Models/4. Chat_HF.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V3.1", task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("Write a poem for Football")
+
+print(result.content)
